88e0eca3eada40d8a188531743d7406e5526a9be
Sciss <contact@sciss.de>
2012-03-05 14:56:51 +0000

    trying to specialize. of course scalac crashes all the time. suckerssssssssss

--- src/main/scala/com/codecommit/collection/FingerTree.scala
+++ src/main/scala/com/codecommit/collection/FingerTree.scala
@@ -4,7 +4,7 @@ package com.codecommit.collection
  * Slight updates for Scala 2.9.1 by Hanns Holger Rutz.
  */
 object FingerTree {
-  sealed trait FingerTree[+A] {
+  sealed trait FingerTree[@specialized(Int, Long, Float, Double, Char) +A] {
     def isEmpty: Boolean
 
     def headLeft: A
@@ -13,8 +13,8 @@ object FingerTree {
     def headRight: A
     def tailRight: FingerTree[A]
 
-    def +:[B >: A](b: B): FingerTree[B]
-    def +[B >: A](b: B): FingerTree[B]
+    def +:[/* @specialized(Int, Long, Float, Double, Char) */ B >: A](b: B): FingerTree[B]
+    def +[/* @specialized(Int, Long, Float, Double, Char) */ B >: A](b: B): FingerTree[B]
 
     def viewLeft: FTViewLeft[FingerTree, A]
     def viewRight: FTViewRight[FingerTree, A]
@@ -22,7 +22,7 @@ object FingerTree {
     def iterator: Iterator[A]
   }
 
-  case class Single[+A](a: A) extends FingerTree[A] {
+  final case class Single[@specialized(Int, Long, Float, Double, Char) +A](a: A) extends FingerTree[A] {
     val headLeft = a
     val tailLeft = Empty
 
@@ -31,9 +31,9 @@ object FingerTree {
 
     val isEmpty = false
 
-    def +:[B >: A](b: B) = Deep(One(b), Empty, One(a))
+    def +:[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Deep(One(b), Empty, One(a))
 
-    def +[B >: A](b: B) = Deep(One(a), Empty, One(b))
+    def +[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Deep(One(a), Empty, One(b))
 
     def viewLeft = FTConsLeft[FingerTree, A](a, Empty)
     def viewRight = FTConsRight[FingerTree, A](Empty, a)
@@ -50,7 +50,11 @@ object FingerTree {
     override def toString = "FingerTree(Single(%s))".format(a)
   }
 
-  case class Deep[+A](prefix: Digit[A], tree: FingerTree[Node[A]], suffix: Digit[A]) extends FingerTree[A] {
+  final case class Deep[@specialized(Int, Long, Float, Double, Char) +A](prefix: Digit[A],
+                                                                         tree: FingerTree[Node[A]],
+                                                                         suffix: Digit[A])
+    extends FingerTree[A] {
+
     val isEmpty = false
 
     val headLeft = prefix.headLeft
@@ -59,12 +63,12 @@ object FingerTree {
     def tailLeft = viewLeft.tail
     def tailRight = viewRight.tail
 
-    def +:[B >: A](b: B) = prefix match {
+    def +:[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = prefix match {
       case Four(d, e, f, g) => Deep(Two(b, d), Node3(d, e, f) +: tree, suffix)
       case partial => Deep(b :: partial, tree, suffix)
     }
 
-    def +[B >: A](b: B) = suffix match {
+    def +[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = suffix match {
       case Four(g, f, e, d) => Deep(prefix, tree + Node3(g, f, e), Two(d, b))
       case partial => Deep(prefix, tree, partial + b)
     }
@@ -115,9 +119,9 @@ object FingerTree {
     def headRight = throw new NoSuchElementException("headRight on empty finger tree")
     def tailRight = throw new NoSuchElementException("tailRight on empty finger tree")
 
-    def +:[A](a: A) = Single(a)
+    def +:[@specialized(Int, Long, Float, Double, Char) A](a: A) = Single(a)
 
-    def +[A](a: A) = Single(a)
+    def +[@specialized(Int, Long, Float, Double, Char) A](a: A) = Single(a)
 
     def viewLeft = FTNilLeft[FingerTree]()
     def viewRight = FTNilRight[FingerTree]()
@@ -132,12 +136,12 @@ object FingerTree {
   }
 
 
-  sealed trait Node[+A] {
+  sealed trait Node[@specialized(Int, Long, Float, Double, Char) +A] {
     def toDigit: Digit[A]
     def toList: List[A]
   }
 
-  case class Node2[+A](a1: A, a2: A) extends Node[A] {
+  final case class Node2[@specialized(Int, Long, Float, Double, Char) +A](a1: A, a2: A) extends Node[A] {
     def toDigit = Two(a1, a2)
 
     def toList = List(a1, a2)
@@ -145,7 +149,9 @@ object FingerTree {
     override def toString = "Node2(%s, %s)".format(a1, a2)
   }
 
-  case class Node3[+A](a1: A, a2: A, a3: A) extends Node[A] {
+  final case class Node3[@specialized(Int, Long, Float, Double, Char) +A](a1: A, a2: A, a3: A)
+    extends Node[A] {
+
     def toDigit = Three(a1, a2, a3)
 
     def toList = List(a1, a2, a3)
@@ -154,56 +160,58 @@ object FingerTree {
   }
 
 
-  sealed trait FTViewLeft[+S[+_], +A] {
+  sealed trait FTViewLeft[+S[+_], /* @specialized(Int, Long, Float, Double, Char) */ +A] {
     def head: A
     def tail: S[A]
   }
 
-  case class FTConsLeft[+S[+_], +A](head: A, tail: S[A]) extends FTViewLeft[S, A]
+  final case class FTConsLeft[+S[+_], /* @specialized(Int, Long, Float, Double, Char) */ +A](head: A, tail: S[A])
+    extends FTViewLeft[S, A]
 
-  case class FTNilLeft[+S[+_]]() extends FTViewLeft[S, Nothing] {
+  final case class FTNilLeft[+S[+_]]() extends FTViewLeft[S, Nothing] {
     def head = throw new NoSuchElementException("head on empty view")
     def tail = throw new NoSuchElementException("tail on empty view")
   }
 
 
-  sealed trait FTViewRight[+S[+_], +A] {
+  sealed trait FTViewRight[+S[+_], /* @specialized(Int, Long, Float, Double, Char) */ +A] {
     def tail: S[A]
     def head: A
   }
 
-  case class FTConsRight[+S[+_], +A](tail: S[A], head: A) extends FTViewRight[S, A]
+  final case class FTConsRight[+S[+_], /* @specialized(Int, Long, Float, Double, Char) */ +A](tail: S[A], head: A)
+    extends FTViewRight[S, A]
 
-  case class FTNilRight[+S[+_]]() extends FTViewRight[S, Nothing] {
+  final case class FTNilRight[+S[+_]]() extends FTViewRight[S, Nothing] {
     def tail = throw new NoSuchElementException("tail on empty view")
     def head = throw new NoSuchElementException("head on empty view")
   }
 
 
-  sealed trait Digit[+A] {
+  sealed trait Digit[/* @specialized(Int, Long, Float, Double, Char) */ +A] {
     def headLeft: A
     def tailLeft: Digit[A]
 
     def headRight: A
     def tailRight: Digit[A]
 
-    def ::[B >: A](b: B): Digit[B]
-    def +[B >: A](b: B): Digit[B]
+    def ::[/* @specialized(Int, Long, Float, Double, Char) */ B >: A](b: B): Digit[B]
+    def +[/* @specialized(Int, Long, Float, Double, Char) */ B >: A](b: B): Digit[B]
 
     def toTree: FingerTree[A]
 
     def iterator: Iterator[A]
   }
 
-  case class One[+A](a1: A) extends Digit[A] {
+  final case class One[@specialized(Int, Long, Float, Double, Char) +A](a1: A) extends Digit[A] {
     val headLeft = a1
     def tailLeft = throw new NoSuchElementException("tail on digit: one")
 
     val headRight = a1
     def tailRight = throw new NoSuchElementException("tail on digit: one")
 
-    def ::[B >: A](b: B) = Two(b, a1)
-    def +[B >: A](b: B) = Two(a1, b)
+    def ::[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Two(b, a1)
+    def +[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Two(a1, b)
 
     def toTree = Single(a1)
 
@@ -217,45 +225,52 @@ object FingerTree {
     }
   }
 
-  case class Two[+A](a1: A, a2: A) extends Digit[A] {
+  final case class Two[@specialized(Int, Long, Float, Double, Char) +A](a1: A, a2: A) extends Digit[A] {
     val headLeft = a1
     def tailLeft = One(a2)
 
     val headRight = a2
     def tailRight = One(a1)
 
-    def ::[B >: A](b: B) = Three(b, a1, a2)
-    def +[B >: A](b: B) = Three(a1, a2, b)
+    def ::[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Three(b, a1, a2)
+    def +[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Three(a1, a2, b)
 
     def toTree = a1 +: Single(a2)
 
     def iterator = (a1 :: a2 :: Nil).iterator
   }
 
-  case class Three[+A](a1: A, a2: A, a3: A) extends Digit[A] {
+  final case class Three[@specialized(Int, Long, Float, Double, Char) +A](a1: A, a2: A, a3: A)
+    extends Digit[A] {
+
     val headLeft = a1
     def tailLeft = Two(a2, a3)
 
     val headRight = a3
     def tailRight = Two(a1, a2)
 
-    def ::[B >: A](b: B) = Four(b, a1, a2, a3)
-    def +[B >: A](b: B) = Four(a1, a2, a3, b)
+    def ::[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Four(b, a1, a2, a3)
+    def +[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) = Four(a1, a2, a3, b)
 
     def toTree = a1 +: a2 +: Single(a3)
 
     def iterator = (a1 :: a2 :: a3 :: Nil).iterator
   }
 
-  case class Four[+A](a1: A, a2: A, a3: A, a4: A) extends Digit[A] {
+  final case class Four[@specialized(Int, Long, Float, Double, Char) +A](a1: A, a2: A, a3: A, a4: A)
+    extends Digit[A] {
+
     val headLeft = a1
     def tailLeft = Three(a2, a3, a4)
 
     val headRight = a4
     def tailRight = Three(a1, a2, a3)
 
-    def ::[B >: A](b: B) = throw new UnsupportedOperationException(":: on Four")
-    def +[B >: A](b: B) = throw new UnsupportedOperationException("+ on Four")
+    def ::[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) =
+      throw new UnsupportedOperationException(":: on Four")
+
+    def +[@specialized(Int, Long, Float, Double, Char) B >: A](b: B) =
+      throw new UnsupportedOperationException("+ on Four")
 
     def toTree = a1 +: a2 +: a3 +: Single(a4)
 
